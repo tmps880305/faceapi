2b840b8e810b0e88ddc4840a0adf7986692039bf
randy <tmps880305@gmail.com>
2019-04-23 10:28:04 +0800

    first commit .

---
+++ src/main/java/com/oppo/faceapi/FaceapiApplication.java
@@ -0,0 +1,13 @@
+package com.oppo.faceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceapiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/oppo/faceapi/api/webcam.java
@@ -0,0 +1,16 @@
+package com.oppo.faceapi.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class webcam {
+    @GetMapping("/webcam")
+    public String changePageSize(@RequestParam(required = false, defaultValue = "1") Integer page,
+                                 @RequestParam(required = false, defaultValue = "5") Integer pageSize, Model model) {
+
+        return "webcam";
+    }
+}
